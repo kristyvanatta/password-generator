d4f701bafc60605f1ace548b1100711c342db449
Kristy Vanatta <kristylvanatta@gmail.com>
2022-08-30 23:29:17 -0400

    completed READme file

--- Assets/script.js
+++ Assets/script.js
@@ -1,10 +1,15 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var password = document.getElementById("#password");
+var password = 128;
+var password = "";
+
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V","W", "X", "Y", "Z"];
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v","w", "x", "y", "z"];
 var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var specialCharacters = ["/", "[", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+",  "=", ];
+
 
 
 // Write password to the #password input
@@ -21,6 +26,7 @@ generateBtn.addEventListener("click", writePassword);
 
 window.prompt("How many characters would you like your password to contain?");
 
+
 if (window.confirm("Click to confirm to include special characters.")){
 
 }
@@ -31,5 +37,5 @@ if (window.confirm("click to confirm to include lowercase characters.")){
 
 }
 if (window.confirm("click to confirm to include uppercase characters.")){
-  
+
 }
--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 # password-generator
+-Randomly generated password.
+-When the button is clicked a prompt box opens and asks for criteria for the password ie. length of password, special characters, numeric, lowercase, and uppercase characters. 
+-The input will be validated.
+-Once the criteria has been selected the password will be displayed in an alert.
 
 ## Screen-shot of page
 <img src= "Assets\images\03-javascript-homework-demo.png"/>
+
+## GitHub links
+https://github.com/kristyvanatta/password-generator
+https://kristyvanatta.github.io/password-generator/
