d990326aafc53c5486dd32f5333b7105617c316f
Kristy Vanatta <kristylvanatta@gmail.com>
2022-08-30 21:53:40 -0400

    added prompts and confims

--- Assets/script.js
+++ Assets/script.js
@@ -5,6 +5,8 @@ var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v","w", "x", "y", "z"];
 var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var specialCharacters = ["/", "[", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+",  "=", ];
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword()
@@ -16,3 +18,18 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+window.prompt("How many characters would you like your password to contain?");
+
+if (window.confirm("Click to confirm to include special characters.")){
+
+}
+if (window.confirm("Click to confirm to include numberic characters.")){
+
+}
+if (window.confirm("click to confirm to include lowercase characters.")){
+
+}
+if (window.confirm("click to confirm to include uppercase characters.")){
+  
+}
