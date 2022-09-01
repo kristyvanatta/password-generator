211dda33e417a01c95949464c3b70a15e25c43db
Kristy Vanatta <kristylvanatta@gmail.com>
2022-09-01 08:54:50 -0400

    added generate password function

--- Assets/script.js
+++ Assets/script.js
@@ -4,7 +4,7 @@
   var password = document.getElementById("#password");
 
 
-function generatePassword() {
+
   var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V","W", "X", "Y", "Z"];
   var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v","w", "x", "y", "z"];
   var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
@@ -12,12 +12,16 @@ function generatePassword() {
   var passwordLength = 128;
   var possibleCharacters =[];
 
+  function generatePassword(upperCase, lowerCase, numbers, specialCharacters) {
+
 for ( var i = 0; i < passwordLength; i++) {
   var password = window.crypto.getRandomValues(writePassword);
-  console.log(password);
+  return password;
+  
 }
 
 }
+//has the user verify the length of password they want
 var passwordLength= window.prompt("How many characters would you like your password to contain?");
 if (passwordLength > 128) {
   alert("Password must be less than 129 characters");
@@ -59,3 +63,4 @@ console.log(password);
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword); 
+
